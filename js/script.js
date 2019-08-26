6065adb8d3164bc89fe04115c970698b2d1292ca
avin99 <avin.rathore@gmail.com>
2019-08-27 01:59:12 +0530

    bouncing and falling added

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@
                 pos++; 
                
               elem.style.left = pos + 'px'; 
-            }
+            } 
             else{
             k--; 
                
@@ -30,8 +30,10 @@
           
     }
 }*/
-
-var f3=0;
+var b3=0,b4=0;
+var ch=0,ch2=0;
+var b1=0,b2=0;
+var f5=0,f6=0;
 var ey=620;
 var ex=675;
 var x1=620,x2=220,flag=0,flag2=0,f3=0;
@@ -45,6 +47,7 @@ window.onload = function(){
 
 function myFunction() {
   f3=1;
+  f5=1;
 }
 var canvas = document.getElementById('canvas');
 canvas.width=window.innerWidth;
@@ -71,6 +74,8 @@ basket3.src = "../images/basketgame.png";
     c.drawImage(basket1,x,y,180,150);
     c.drawImage(basket2,x1,y1-gap,180,150);
     c.drawImage(basket3,x2,y2-2*gap,180,150);
+    
+    
     //basket1
     if(x1+180<canvas.width && flag==0){
      x1=x1+5;
@@ -81,29 +86,99 @@ basket3.src = "../images/basketgame.png";
         if(x1==0)
         flag=0;
     }
+
+
+
+
+
     //basket2
     if(x2+180<canvas.width && flag2==0){
-        x2=x2+5;
+        x2=x2+10;
        } else
        {
-        x2=x2-5;
+           x2=x2-10;
            flag2=1;
            if(x2==0)
            flag2=0;
        }
+
+
+
+
+
        if(f3==1){
-           if(f4==0)
+           if(f4==0&&ey>=350 &&ch==0)
+       {
+          // if(ch==0)
         ey-=gravity;
-         if(ey==(y1-gap) && (ex>=x1-30 && ex<=x1+210)){
-             ey=y1-gap;
-             ex=x1+56;
+       } 
+       else{
+        ch=1;
+        ey+=gravity;
+     }
+       
+
+         if(ey<=(y1-gap+30) && (ex>=x1-30 && ex<=x1+210)){
+             if(b1!=1)
+            { ey=y1-gap;
+             ex=x1+56;}
              f4=1;
+             f5=0;
+                  document.getElementById("canvas").onclick = function() {myFunction1()};
+
+             function myFunction1() {
+               b1=1;
+                }
+               
+         }
+
+
+
+
+         if(b1==1){
+            if(b2==0 && ch2==0 && ey>0){
+         ey-=8;
+            }
+            else{
+                ch2=1;
+                ey+=8;
+             }
+         if(ey<=(y2-2*gap+30) && (ex>=x2-30 && ex<=x2+210)){
+            ey=y2-2*gap;
+            ex=x2+56;
+            b2=1;
          }
        }
+       
+       }
+       
+
+
+
+
+
+    //    if(f5==1){   
+    //     if(f6==0)
+    //  ey-=gravity;
+    //   if(ey==(y2-2*gap) && (ex>=x2-30 && ex<=x2+210)){
+    //       ey=y2-2*gap;
+    //       ex=x2+56;
+    //       f6=1;
+    //       f4=1;
+    //   }
+    // }
        
 
     requestAnimationFrame(draw);
     }
+    // function movedown(){
+    //     if(ey<620)
+    //     {
+    //         ey+=29;
+    //         f3=0;
+    //     requestAnimationFrame(movedown);
+    //     }
+    // }
 
 // bg.onload = function()
 // {
